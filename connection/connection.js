52694b16b1c172a8a1f01904ef26880adece650c
rakesh <rakeshjson@gmail.com>
2023-03-30 18:38:30 +0530

    removed uncesaaary lines

--- connection/connection.js
+++ connection/connection.js
@@ -3,13 +3,7 @@ mongoose.set(`strictQuery`, true)
 async function getConnection() {
     const uri = `mongodb+srv://rakeshjson:rakesh123456@cluster0.5nulz9g.mongodb.net/test`
     await mongoose.connect(uri, (err)=>{
-        // console.log( err ? "Connection to mongoDB failed" : "Connected to MongoDB successfully" ) 
-        if(err){
-           console.log("Connection to mongoDB failed") 
-        }
-        else{
-            console.log("Connected to DataBase!!")
-        }
+        console.log( err ? "Connection to mongoDB failed" : "Connected to MongoDB successfully" ) 
     })
 }
 module.exports = getConnection
